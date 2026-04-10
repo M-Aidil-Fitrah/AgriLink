e427871217bd9d84630f8411193bc12977acc6bf
Muhammad Aidil Fitrah <muh.aidil@mhs.usk.ac.id>
2026-04-10 23:12:20 +0700

    fix: folder id tidak terbaca

--- src/app/dashboard/produk/[id]/page.tsx
+++ src/app/dashboard/produk/[id]/page.tsx
@@ -3,9 +3,11 @@ import { ProductDetails } from "@/components/dashboard/buyer/ProductDetails";
 import { notFound } from "next/navigation";
 import { ProductWithFarmer } from "@/lib/types";
 
-export default async function ProductDetailPage({ params }: { params: { id: string } }) {
+export default async function ProductDetailPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  
   const product = await prisma.product.findUnique({
-    where: { id: params.id },
+    where: { id },
     include: {
       farmer: {
         select: {
